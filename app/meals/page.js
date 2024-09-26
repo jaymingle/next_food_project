7a12d3f94cfd56b09d676cfcfaef6a7cf215b728
Jay Mingle <jkawudi@gmail.com>
2024-09-26 17:14:42 +0000

    Changes

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,18 @@
 import Link from "next/link";
 import classes from './page.module.css'
+import MealsGrid from "@/components/meals/meals-grid";
+import {getMeals} from "@/lib/meals";
+import {Suspense} from "react";
 
-const MealsPage = () => {
+async function Meals(){
+
+    const meals = await getMeals();
+    return  <MealsGrid meals={meals}/>
+
+}
+
+const MealsPage = async() => {
+
     return (
         <>
             <header className={classes.header}>
@@ -11,7 +22,11 @@ const MealsPage = () => {
                     <Link href="/meals/share">Share Your Meals</Link>
                 </p>
             </header>
-            <main className={classes.main}></main>
+            <main className={classes.main}>
+                <Suspense fallback={<p className={classes.loading}>Fetching Meals....</p>}>
+                    <Meals/>
+                </Suspense>
+            </main>
         </>
     )
 }
