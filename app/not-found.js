9ea5ba6d075895ec9cc97b77e9dd3546f79a01f2
Jay Mingle <jkawudi@gmail.com>
2024-09-26 17:32:13 +0000

    Changes

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return <main className='not-found'>
+        <h1>Not Found!!</h1>
+        <p>Sorry, we can not found the page or resource you are looking for</p>
+
+    </main>
+}
+
+export default NotFound;
