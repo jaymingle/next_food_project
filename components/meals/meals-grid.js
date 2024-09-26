fe3b3673d8627edad05164e2409fd252966e8c54
Jay Mingle <jkawudi@gmail.com>
2024-09-26 10:47:18 +0000

    Changes

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './meals-grid.module.css'
+import MealItem from "@/components/meals/meal-item";
 
 
 const MealsGrid = ({meals}) => {
     return (
         <ull className={classes.meals}>
             {meals.map(meal => <li key={meal.id}>
-
+                {<MealItem {...meal} />}
             </li>)}
         </ull>
     );
