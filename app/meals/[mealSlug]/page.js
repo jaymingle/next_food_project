d2bbe66740c1eeac9aa2901ca17eb9c295e73afd
Jay Mingle <jkawudi@gmail.com>
2024-09-27 00:21:06 +0000

    Changes

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import classes from './page.module.css'
 
 const MealDetails = ({params}) => {
     return (
-        <main>
-            <h1>Meals Page</h1>
-            <p>This a meal with an ID: {params.id}</p>
-        </main>
+        <>
+            <header>
+
+            </header>
+            <main>
+                <h1>Meals Page</h1>
+                <p>This a meal with an ID: {params.slug}</p>
+            </main>
+        </>
     );
 };
 
